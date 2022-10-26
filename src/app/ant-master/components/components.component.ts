0f76622e02cdd805fb0b654d62eee560ef0a7309
= <=>
2022-10-26 15:04:56 +0300

    components page

--- src/app/ant-master/components/components.component.ts
+++ src/app/ant-master/components/components.component.ts
@@ -1,10 +1,17 @@
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+import { NzStepsModule } from 'ng-zorro-antd/steps';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-components',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    NzProgressModule,
+    NzStepsModule,
+    NzDividerModule
+  ],
   templateUrl: './components.component.html',
   styleUrls: ['./components.component.scss']
 })
--- src/app/ant-master/dashboard/dashboard.component.ts
+++ src/app/ant-master/dashboard/dashboard.component.ts
@@ -1,10 +1,23 @@
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzStatisticModule } from 'ng-zorro-antd/statistic';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+import { NzTimelineModule } from 'ng-zorro-antd/timeline';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NzGridModule,
+    NzCardModule,
+    NzStatisticModule,
+    NzProgressModule,
+    NzTimelineModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
